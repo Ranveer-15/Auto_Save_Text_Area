79ab40930db1598f0c3206e0fe031132fc30faa7
Ranveer-15 <ranveersingh.150101@gmail.com>
2025-03-15 20:56:34 +0530

    Local storage

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("textarea");
+const select = document.querySelector("select");
+
+window.addEventListener("load", (e)=>{
+    if(localStorage.getItem("data")){
+        input.value = localStorage.getItem("data");
+    }
+    // set theme color from local storage
+    input.className  = localStorage.getItem("theme");
+});
+
+input.addEventListener("focusout", (e)=>{
+    localStorage.setItem("data", input.value);
+});
+
+select.addEventListener("change", (e)=>{
+    input.className = select.value;
+    // save theme value to local storage
+    localStorage.setItem("theme", select.value)
+})
+
+
+
